95d4c39e6a2b07c5eb029482860a85167c82ae73
UnNou <c3noun00@students.oamk.fi>
2024-12-17 22:38:27 +0200

    .env

--- pgserver.js
+++ pgserver.js
@@ -1,14 +1,17 @@
 import express from 'express';
 import pg from 'pg';
+import dotenv from 'dotenv';
+
+dotenv.config(); 
 
 const app = express();
 
 const client = new pg.Client({
-    user: 'postgres',
-    password: 'SuolaGurgu@5521',
-    database: 'postgres',
-    host: 'localhost',
-    port: 5432,
+       user: process.env.PG_USER,
+    password: process.env.PG_PW,
+    database: process.env.PG_DB,
+    host: process.env.PG_HOST,
+    port: process.env.PG_PORT 
 });
 
 client.connect()
